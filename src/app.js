bc8dd4e7ea1742b55d3389de8a4045ae39c52495
Matheus <matheus.ag9610@gmail.com>
2024-05-04 20:09:06 -0300

    Finsh

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import routes from "./routes/index.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+  console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+  console.log("Conexao com o banco feita com sucesso");
+});
+
+const app = express();
+routes(app);
+
+app.get("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("livro cadastrado com sucesso");
+});
+
+app.put("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.status(200).json(livros);
+});
+
+app.delete("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("livro removido com sucesso");
+});
+
+export default app;
---
+++ src/config/dbConnect.js
@@ -0,0 +1,9 @@
+import mongoose, { mongo } from "mongoose";
+
+async function conectaNaDatabase() {
+mongoose.connect(process.env.DB_CONNECTION_STRING);
+return mongoose.connection;
+};
+
+export default conectaNaDatabase;
+
